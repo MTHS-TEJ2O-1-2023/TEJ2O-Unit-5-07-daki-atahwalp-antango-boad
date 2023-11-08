e96950e32fb597a5a192c99fac1b7d8701de76df
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-08 12:50:01 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Nov 2023
+ * This program controls a servo through Robotbit
 */
 
-basic.showString('Hello, World!')
+// variables
+const servo1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servo1, 0)
+  basic.clearScreen()
+  basic.showString('0')
+  basic.showIcon(IconNames.SmallSquare)
+  basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servo1, 180)
+  basic.clearScreen()
+  basic.showString('180')
+  basic.showIcon(IconNames.SmallSquare)
+  basic.showIcon(IconNames.Happy)
+})
